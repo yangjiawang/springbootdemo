9f8f52022c19659fbcdc8c254764f39d92ff2623
yangjiawang <yangjiawang@qq,com>
2019-09-29 23:42:10 +0800

    第二次提交

--- src/main/java/com/yangjiawang/SpringbootdemoApplication.java
+++ src/main/java/com/yangjiawang/SpringbootdemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringbootdemoApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringbootdemoApplication.class, args  );
+        SpringApplication.run(SpringbootdemoApplication.class, args   );
     }
 
 }
